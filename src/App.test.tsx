4512311207cdeb24ccc80e7ad965fc7897d2ae7f
V <huynh.h.vy@gmail.com>
2023-10-11 10:56:55 +0700

    basic setup for the project, add sass and remove unused codes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+describe('test suite', () => {
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
 });
+})
